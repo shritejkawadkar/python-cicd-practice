819f6700d25257c02b68ac460f2c5cb4542d1ff6
Ubuntu <ubuntu@ip-172-31-69-45.ec2.internal>
2026-02-27 09:25:16 +0000

    cm2

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# 👇 THIS IS THE FIX (adds project root to Python path)
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
 from app import app
 
 def test_home():
